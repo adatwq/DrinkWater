9d45da9b617d7fed4ed855af950c52a196a4ae39
Khawlah Khalid <khawlah1920@hotmail.com>
2023-12-06 11:46:21 +0300

    Initial Commit

---
+++ DrinkWater/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  DrinkWater
+//
+//  Created by Khawlah Khalid on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
