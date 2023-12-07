43b10f53976521d8db224d54b8b36375eccdb56c
Khawlah Khalid <khawlah1920@hotmail.com>
2023-12-07 08:40:09 +0300

    Interactive widget

---
+++ DrinkWaterWidget/CupsInfo.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  DrinkWater
+//
+//  Created by Khawlah Khalid on 07/12/2023.
+//
+
+import Foundation
+
+
+class CupsInfo{
+    static let shared = CupsInfo()
+    var totalCups: Int = 8
+    var currentCups: Int = 0
+}
---
+++ DrinkWaterWidget/DrinkWaterIntent.swift
@@ -0,0 +1,21 @@
+//
+//  DrinkWaterIntent.swift
+//  DrinkWater
+//
+//  Created by Khawlah Khalid on 06/12/2023.
+//
+
+import Foundation
+import AppIntents
+import SwiftUI
+
+struct DrinkWaterIntent: AppIntent{
+    static var title: LocalizedStringResource = "Drink water"
+    func perform() async throws -> some IntentResult {
+        print("Button taped !")
+        if CupsInfo.shared.currentCups < CupsInfo.shared.totalCups{
+            CupsInfo.shared.currentCups += 1
+        }
+        return .result()
+    }
+}
